9618e88d50a38c240cd4781fff6d1c04d5f10d64
Pol Selles Climent <81540683+Pokesi@users.noreply.github.com>
2023-03-08 14:31:10 +1100

    ➕Begin OmniRave

---
+++ contracts/Core/RaveV3.sol
@@ -0,0 +1,19 @@
+/*
+ * Rave v3
+ * 
+ * A multi-chain name service, powered by layer zero. The storage for the names and 
+ * all their data is done on Fantom, due to the low costs. Users can register .rave 
+ * names in either >=100 RAVE or the equivalent in the chain's native token. The 
+ * plus-side to multichain names is that anyone can resolve a name on any chain, 
+ * natively. This allows for developers to use .rave names in the stead of addresses 
+ * as a sort of verification, or anti-bot measure.
+ * 
+ * Rave v3 will be compatiable with any ENS-enabled wallet through ENS sub-domains
+ * (similar to the current .ftm.fyi solution), just with an on-chain resolver as a
+ * pose to an off-chain one.
+ */
+
+
+contract OmniRave {
+    
+}
